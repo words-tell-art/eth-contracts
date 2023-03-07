52fa8d65c0f5309865b7e425d10b38ff35095411
Draym <kevin.draym@gmail.com>
2023-03-07 16:43:00 +0800

    task: update ArtNft with new requirements

--- contracts/ArtNft.sol
+++ contracts/ArtNft.sol
@@ -1,50 +1,36 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-import "./opensea/ERC721Tradable.sol";
 import "./NFT.sol";
 import "./interfaces/IERC721Drop.sol";
-import "./InputVerifier.sol";
 
-contract ArtNft is NFT, InputVerifier {
+contract ArtNft is NFT {
+    event CraftEvent(uint256 idCraft, uint256 idBurned, uint256 idBurned2, uint256 idBurned3, uint256 idBurned4, uint256 idBurned5);
     event MergeEvent(uint256 idMerged, uint256 idBurned);
 
-    struct Data {
-        string w1;
-        string w2;
-        string w3;
-        string w4;
-        string w5;
-    }
-
     IERC721Drop _wordNft;
-    mapping(uint256 => Data) private _nfts;
 
-    constructor(address wordNft, address proxyRegistryAddress, address signer) NFT("Words Tell Art", "ART", proxyRegistryAddress) InputVerifier(signer) {
+    constructor(address wordNft, address proxyRegistryAddress) NFT("Words Tell Art", "ART", proxyRegistryAddress) {
         _wordNft = IERC721Drop(wordNft);
     }
 
-    function craft(uint256[] memory wordIs, string memory w1, string memory w2, string memory w3, string memory w4, string memory w5, bytes memory signature) external {
+    function craft(uint256 w1, uint256 w2, uint256 w3, uint256 w4, uint256 w5) external {
         require(_wordNft.isApprovedForAll(_msgSender(), address(this)), "ART contract not approved");
-        bytes32 messageHash = keccak256(abi.encodePacked(wordIs, w1, w2, w3, w4, w5));
-        _verifyMessage(messageHash, signature);
-        address caller = _wordNft.ownerOf(wordIs[0]);
         uint max = wordIs.length;
         for (uint i = 0; i < max; i++) {
             _wordNft.burn(wordIs[i]);
         }
-        _mintTo(caller);
+        _mintTo(msg.sender, w1);
+        emit CraftEvent(w1, w1, w2, w3, w4, w5);
+    }
+
+    function _mintTo(address _to, uint256 _tokenId) internal override {
+        _safeMint(_to, _tokenId);
     }
 
     function merge(uint256 art1, uint256 art2) external {
         require(_isApprovedOrOwner(msg.sender, art1), "Caller is not token owner");
         burn(art2);
-        _nfts[art1].w2 = _nfts[art2].w2;
-        _nfts[art1].w4 = _nfts[art2].w4;
         emit MergeEvent(art1, art2);
-    }
-
-    function setSigner(address signer) external onlyMaintainer {
-        _setSigner(signer);
     }
 }
