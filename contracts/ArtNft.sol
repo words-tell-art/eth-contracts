ff517c3ea6828818b3f08c06eeb11fbad21e29f5
Draym <kevin.draym@gmail.com>
2023-03-07 17:11:20 +0800

    task: update Art craft

--- contracts/ArtNft.sol
+++ contracts/ArtNft.sol
@@ -5,7 +5,7 @@ import "./NFT.sol";
 import "./interfaces/IERC721Drop.sol";
 
 contract ArtNft is NFT {
-    event CraftEvent(uint256 idCraft, uint256 idBurned, uint256 idBurned2, uint256 idBurned3, uint256 idBurned4, uint256 idBurned5);
+    event CraftEvent(uint256 idCraft, uint256[] idBurned);
     event MergeEvent(uint256 idMerged, uint256 idBurned);
 
     IERC721Drop _wordNft;
@@ -14,14 +14,15 @@ contract ArtNft is NFT {
         _wordNft = IERC721Drop(wordNft);
     }
 
-    function craft(uint256 w1, uint256 w2, uint256 w3, uint256 w4, uint256 w5) external {
+    function craft(uint256[] wordIs) external {
         require(_wordNft.isApprovedForAll(_msgSender(), address(this)), "ART contract not approved");
+        require(wordIs.length >= 2, "Require at least 2 words.");
         uint max = wordIs.length;
         for (uint i = 0; i < max; i++) {
             _wordNft.burn(wordIs[i]);
         }
-        _mintTo(msg.sender, w1);
-        emit CraftEvent(w1, w1, w2, w3, w4, w5);
+        _mintTo(msg.sender, wordIs[0]);
+        emit CraftEvent(wordIs[0], wordIs);
     }
 
     function _mintTo(address _to, uint256 _tokenId) internal override {
